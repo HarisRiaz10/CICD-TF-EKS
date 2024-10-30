659b1e18d9daf2fed9d9da13305b6d3bf5a3529d
HarisRiaz10 <90620838+HarisRiaz10@users.noreply.github.com>
2024-10-30 16:18:33 +0500

    Update main.tf

--- modules/EKS/main.tf
+++ modules/EKS/main.tf
@@ -23,7 +23,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
+    instance_types = ["t3.medium"]
 
     attach_cluster_primary_security_group = true
   }
@@ -34,7 +34,7 @@ module "eks" {
       max_size     = 5  # Keep the max size low for cost savings
       desired_size = 3
 
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"  # Use SPOT instances for further savings
     }
   }
